a1288a9903e8b7cebe105777a2816abde9cce324
dgyoon <douglasyoon9@gmail.com>
2023-03-02 16:23:48 +0900

    페이지 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Suspense } from 'react';
 import './App.css';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
@@ -20,35 +20,39 @@ import KakaoLogin from './pages/KakaoLogin';
 import SetListType from './pages/SetListType';
 import PType from './components/setting/PType';
 import PrivateRoute from './PrivateRoute';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
     <RecoilRoot>
       <Suspense fallback={<div>Loading...</div>}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Routes>
-        {/* <Route element={<PrivateRoute authentication={false} />}> */}
-        <Route path={'/auth'} element={<Auth />} />
-        <Route path={'/auth/kakao'} element={<KakaoLogin />} />
-        <Route path={'/register'} element={<Register />} />
-        <Route path={'/login'} element={<Login />} />
-         {/* </Route> */}
-        {/* <Route element={<PrivateRoute authentication={true} />}> */}
-        <Route path={'/'} element={<List />} />
-        <Route path={'/calendar'} element={<Calendar />} />
-        <Route path={'/stats'} element={<Stats />} />
-        <Route path={'/setting'} element={<Setting />} />
-        <Route path={'/setlisttype'} element={<SetListType />} />
-        <Route path={'/setpayment'} element={<SetPayment />} />
-        <Route path={'/ptype/:name'} element={<PType />} />
-        <Route path={'/setcategory'} element={<SetCategory />} />
-        <Route path={'/setcategory/:no/:name'} element={<SettingCdclist />} />
-        <Route path={'/write'} element={<Write />} />
-        <Route path={'*'} element={<Navigate to='/' />} />
-                  {/* </Route> */}
-        </Routes>
-      </ThemeProvider>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Routes>
+            <Route element={<PrivateRoute authentication={false} />}>
+              <Route path={'/auth'} element={<Auth />} />
+              <Route path={'/auth/kakao'} element={<KakaoLogin />} />
+              <Route path={'/register'} element={<Register />} />
+              <Route path={'/login'} element={<Login />} />
+            </Route>
+            <Route element={<PrivateRoute authentication={true} />}>
+              <Route path={'/'} element={<List />} />
+              <Route path={'/calendar'} element={<Calendar />} />
+              <Route path={'/stats'} element={<Stats />} />
+              <Route path={'/setting'} element={<Setting />} />
+              <Route path={'/setlisttype'} element={<SetListType />} />
+              <Route path={'/setpayment'} element={<SetPayment />} />
+              <Route path={'/ptype/:name'} element={<PType />} />
+              <Route path={'/setcategory'} element={<SetCategory />} />
+              <Route
+                path={'/setcategory/:no/:name'}
+                element={<SettingCdclist />}
+              />
+              <Route path={'/write'} element={<Write />} />
+              <Route path={'*'} element={<Navigate to='/' />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
       </Suspense>
     </RecoilRoot>
   );
